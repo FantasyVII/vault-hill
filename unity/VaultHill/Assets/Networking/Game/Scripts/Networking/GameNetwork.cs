59213e3bb739f1ad92de12d2c22fcf62c0083121
Mustafa Sibai <contact@m-sibai.com>
2022-01-09 10:43:56 +0400

    integrated client with unity

--- unity/VaultHill/Assets/Networking/Game/Scripts/Networking/GameNetwork.cs
+++ unity/VaultHill/Assets/Networking/Game/Scripts/Networking/GameNetwork.cs
@@ -3,12 +3,16 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
+using GameNetworkLib;
+using GameNetworkLib.Packets;
+using GameNetworkLib.Packets.Mutual;
 
 public class GameNetwork : MonoBehaviour
 {
     static GameNetwork instance;
 
     Socket socket;
+    Player player;
 
     bool runNetworkLoop;
 
@@ -31,10 +35,12 @@ public class GameNetwork : MonoBehaviour
     public void ConnectToServer(string username)
     {
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000));
+        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000);
+        socket.Connect(ipEndPoint);
         socket.Blocking = false;
-        //byte[] recieveBuffer = new byte[1024];
-        socket.Send(Encoding.ASCII.GetBytes("Hello"));
+        player = new Player(Guid.NewGuid().ToString("N"), "Vivi", ipEndPoint);
+
+        socket.Send(new ConnectPacket().PrepareRequest(player).Serialize());
         runNetworkLoop = true;
     }
 
@@ -43,51 +49,54 @@ public class GameNetwork : MonoBehaviour
         if (!runNetworkLoop)
             return;
 
-        try
-        {
-            if (socket.Available > 0)
-            {
-                /*byte[] recieveBuffer = new byte[socket.Available];
-                socket.Receive(recieveBuffer);
 
-                BasePacket bp = new BasePacket();
-                bp.Deserialize(recieveBuffer);
+        if (socket.Available > 0)
+        {
+            try
+            {
+                byte[] receivedBuffer = new byte[socket.Available];
+                socket.Receive(receivedBuffer);
+
+                BasePacket bp = new BasePacket().Deserialize(receivedBuffer);
+
+                switch (bp.NetworkEvents)
+                {
+                    case PacketEvents.AlivePing:
+                        print("Recieved Ping");
+                        socket.Send(new PingPacket().SuccessResponse(player).Serialize());
+                        break;
+                    case PacketEvents.ConnectToServer:
+                        break;
+                    case PacketEvents.DisconnectFromServer:
+                        break;
+                    default:
+                        break;
+                }
 
                 switch (bp.Response)
                 {
                     case PacketResponse.ConnectedToServerSuccessfully:
-                        ConnectedToServer();
-                        break;
-
+                        {
+                            print("Connected to server!");
+                            break;
+                        }
                     case PacketResponse.FailedToConnectToServer:
-                        FailedToConnectToServer();
                         break;
 
+                    case PacketResponse.DisconnectedFromServerSuccessfully:
+                        break;
+
+                    case PacketResponse.FailedToDisconnectFromServer:
+                        break;
                     default:
                         break;
                 }
-
-                //---------------------------------------------------------------------------------------------------------------------
-                //---------------------------------------------------------------------------------------------------------------------
-
-                switch (bp.Request)
-                {
-                    case PacketRequest.GlobalChatMessage:
-                        GlobalMessagePacket gmp = new GlobalMessagePacket().Deserialize(recieveBuffer);
-                        socket.Send(new GlobalMessagePacket().SuccessResponse(player).Serialize());
-                        RecievedChatMessage(gmp.Player.Name, gmp.Message);
-                        break;
-
-                    default:
-                        break;
-                }
-            }*/
             }
-        }
-        catch (SocketException ex)
-        {
-            if (ex.SocketErrorCode != SocketError.WouldBlock)
-                Debug.LogError(ex);
+            catch (SocketException ex)
+            {
+                if (ex.SocketErrorCode != SocketError.WouldBlock)
+                    Debug.LogError(ex);
+            }
         }
     }
 }
